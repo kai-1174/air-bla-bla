859bca78850b4838c99302731e1b7741d2af40f2
kai-1174 <cartooncraze52@gmail.com>
2025-07-11 14:43:56 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+ javascript
+const accountBtn = document.getElementById("accountBtn");
+const accountDropdown = document.getElementById("accountDropdown");
+const hamburger = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobileMenu");
+[2:04 PM, 7/11/2025] ChatGPT: accountBtn.addEventListener("click", () => {
+  accountDropdown.classList.toggle("hidden");
+});
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+```
